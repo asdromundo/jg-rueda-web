f0fe80f97307ae435124bd65048f70889a7043aa
Antonio S. Dromundo <sebastiandromundo@outlook.com>
2023-10-30 17:19:30 -0600

    Added light/dark theme toogle

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,60 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import { ref } from 'vue';
+import { ref } from 'vue'
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+
+function toggleTheme() {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark';
+}
 
 let data = ref({
   drawer: false,
   showButton: false,
-  drawerButtonShape: "rounded"
+  drawerButtonShape: 'rounded'
 })
 </script>
 
 <template>
-  <v-card>
-    <v-layout class="rounded rounded-md">
-      <v-app-bar
-        color="teal-darken-4"
-        permanent
-      >
-        <template v-slot:prepend >
-          <v-app-bar-nav-icon
+  <v-app ref="app" class="rounded rounded-md bg-surface-variant">
+    <v-app-bar color="" permanent>
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon
           :class="data.drawerButtonShape"
           @click.stop="data.drawer = !data.drawer"
-          class="d-md-none"
-          />
-        </template>
+          class="d-sm-none"
+        />
+      </template>
 
-        <v-app-bar-title>Juan Gustavo Rueda Escobedo</v-app-bar-title>
+      <v-app-bar-title>Juan Gustavo Rueda Escobedo</v-app-bar-title>
 
-        <div class="d-none d-md-block">
-          <v-btn to="/" variant="text" class="text-none text-subtitle-1">Home</v-btn>
-          <v-btn to="/publications" variant="text" class="text-none text-subtitle-1">Publications</v-btn>
-          <v-btn to="/about" variant="text" class="text-none text-subtitle-1">About</v-btn>
-          <v-btn to="/contact" variant="text" class="text-none text-subtitle-1">Contact</v-btn>
-        </div>
+      <div class="d-none d-sm-block text-blue-darken-3">
+        <v-btn to="/" variant="text" class="text-none text-subtitle-1 mr-1">Home</v-btn>
+        <v-btn to="/publications" variant="text" class="text-none text-subtitle-1 mr-1">Publications</v-btn>
+        <v-btn to="/about" variant="text" class="text-none text-subtitle-1 mr-1">About</v-btn>
+        <v-btn to="/contact" variant="text" class="text-none text-subtitle-1 mr-1">Contact</v-btn>
+        <v-btn icon="mdi-theme-light-dark" @click="toggleTheme"></v-btn>
+      </div>
+    </v-app-bar>
 
-      </v-app-bar>
+    <v-navigation-drawer v-model="data.drawer">
+      <v-list-item title="Site" subtitle="Navegation"></v-list-item>
+      <v-divider></v-divider>
+      <v-list-item to="/" title="Home"></v-list-item>
+      <v-list-item to="/publications" title="Publications"></v-list-item>
+      <v-list-item to="/about" title="About"></v-list-item>
+      <v-list-item to="/contact" title="Contact"></v-list-item>
+    </v-navigation-drawer>
 
-      <v-navigation-drawer
-      v-model="data.drawer">
-        <v-list-item title="Site" subtitle="Navegation"></v-list-item>
-        <v-divider></v-divider>
-        <v-list-item to="/" title="Home"></v-list-item>
-        <v-list-item to="/publications" title="Publications"></v-list-item>
-        <v-list-item to="/about" title="About"></v-list-item>
-        <v-list-item to="/contact" title="Contact"></v-list-item>
-      </v-navigation-drawer>
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px">
+      <RouterView />
+    </v-main>
 
-      <v-main
-      class="rounded rounded-md"
-      style="min-height: 300px;"
-      >
-        <RouterView />
-      </v-main>
-    </v-layout>
-  </v-card>
+    <v-footer>
+      <v-btn class="mr-auto" variant="text"> Get data </v-btn>
+    </v-footer>
+  </v-app>
 </template>
+
+
